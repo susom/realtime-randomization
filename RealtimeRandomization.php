6df2ceaa031518bb2d6aa226c8790233340545a3
ihabz <ihab.zeedia@stanford.edu>
2023-12-18 09:31:45 -0800

    revert redcap_data change.

--- RealtimeRandomization.php
+++ RealtimeRandomization.php
@@ -96,8 +96,8 @@ class RealtimeRandomization extends \ExternalModules\AbstractExternalModule {
 
         // I am unable to get the value to save using normal methods... So, I'm doing it manually
         $result = $this->query(
-            "SELECT * FROM ? WHERE project_id=? and record=? and event_id=? and field_name=?",
-            [$data_table, $project_id, $record, $targetEvent, $targetField]
+            "SELECT * FROM $data_table WHERE project_id=? and record=? and event_id=? and field_name=?",
+            [$project_id, $record, $targetEvent, $targetField]
         );
 
         if ($result->num_rows > 0) {
@@ -107,8 +107,8 @@ class RealtimeRandomization extends \ExternalModules\AbstractExternalModule {
         } else {
             // Do an insert
             $result = $this->query(
-                "INSERT INTO ? (project_id, event_id, record, field_name, value) VALUES (? , ? , ? , ? , ?)",
-                [$data_table, $project_id, $event_id, $record, $targetField, $randValue]
+                "INSERT INTO $data_table (project_id, event_id, record, field_name, value) VALUES (? , ? , ? , ? , ?)",
+                [$project_id, $event_id, $record, $targetField, $randValue]
             );
             $this->emDebug("Insert Result", $result);
             REDCap::logEvent($this->getModuleName(),"$targetField = '$randValue'",$result,$record, $event_id);
